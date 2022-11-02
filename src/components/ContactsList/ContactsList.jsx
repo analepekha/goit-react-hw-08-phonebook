5004a1692753a7c7a4fea9277b179cbebdad9638
analepekha <maiboroda0103@gmail.com>
2022-11-02 15:42:09 +0200

    bugfix

--- src/components/ContactsList/ContactsList.jsx
+++ src/components/ContactsList/ContactsList.jsx
@@ -6,38 +6,18 @@ import {
 } from '../ContactsList/ContactsList.styled';
 import { useSelector, useDispatch } from "react-redux";
 import { removeContact } from 'redux/contacts/contacts-slice';
-import { getContacts } from 'redux/contacts/contacts-selectors';
-import { getFilter } from 'redux/filter/filter-selectors';
+import { getFilteredContacts} from 'redux/contacts/contacts-selectors';
 
 export const ContactsList = () => {
 
-    const contacts = useSelector(getContacts);
-    const filter = useSelector(getFilter);
+    const filteredContacts = useSelector(getFilteredContacts);
     const dispatch = useDispatch();
-
 
     const onRemoveContact = (id) => {
         const action = removeContact(id);
         dispatch(action);
         console.log(action);
     }
-
-    const getFilteredContacts = () => {
-        
-        if (!filter) {
-          return contacts;
-        }
-
-        const normalizedFilter = filter.toLocaleLowerCase();
-        const filteredContacts = contacts.filter(({ name, number }) => {
-            const normalizedName = name.toLocaleLowerCase();
-            const resultOfFilter = normalizedName.includes(normalizedFilter) || number.includes(normalizedFilter);
-            return resultOfFilter;
-        })
-        return filteredContacts;
-    }
-
-    const filteredContacts = getFilteredContacts();
     
     return (
         <List>
--- src/redux/contacts/contacts-selectors.js
+++ src/redux/contacts/contacts-selectors.js
@@ -1,3 +1,22 @@
-export const getContacts = store => store.contacts;
+import { getFilter } from 'redux/filter/filter-selectors';
 
+export const getContacts = state => state.contacts;
 
+export const getFilteredContacts = state => {
+
+    const contacts = getContacts(state);
+    const filter = getFilter(state);
+
+    if (!filter) {
+      return contacts;
+    }
+
+    const normalizedFilter = filter.toLocaleLowerCase();
+    const filteredContacts = contacts.filter(({ name, number }) => {
+        const normalizedName = name.toLocaleLowerCase();
+        const resultOfFilter = normalizedName.includes(normalizedFilter) || number.includes(normalizedFilter);
+        return resultOfFilter;
+    })
+    return filteredContacts;
+
+}
