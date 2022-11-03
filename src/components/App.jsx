3aca5f68ae590ae1ba8a1f59cd823d7c3dda38bc
analepekha <maiboroda0103@gmail.com>
2022-11-03 18:02:40 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,6 +14,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 export function App() {
   const contacts = useSelector(selectContacts);
+  console.log(contacts.length);
   const isLoading = useSelector(selectIsLoading);
   const error = useSelector(selectError);
   const dispatch = useDispatch();
